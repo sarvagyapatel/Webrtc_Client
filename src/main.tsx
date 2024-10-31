ada48ad543da804cd27cba2e7ab93f4f3ab64e76
Sarvagya Patel <patelsarvagya5@gmail.com>
2024-11-01 00:02:24 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Sender } from './components/Sender'
 import { Reciever } from './components/Reciever'
-import App from './App'
 
 
 const router = createBrowserRouter([
