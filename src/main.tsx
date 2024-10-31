5032d1bc00c8b9abb43a5bdda674d006b58ebdfb
Sarvagya Patel <patelsarvagya5@gmail.com>
2024-11-01 00:00:09 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,20 @@
 import ReactDOM from "react-dom/client";
 import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import {Sender} from './components/Sender'
-import {Reciever} from './components/Reciever'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Sender } from './components/Sender'
+import { Reciever } from './components/Reciever'
 import App from './App'
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path:"/sender",
-        element: <Sender />
-      },
-      {
-        path: "/receiver",
-        element: <Reciever />
-      }
-    ],
+    path: "/sender",
+    element: <Sender />
   },
+  {
+    path: "/receiver",
+    element: <Reciever />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
